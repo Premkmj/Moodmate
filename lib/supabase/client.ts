ffeb6e44dbca7b796f20ea1b900dfb22e4b0faaf
Bishal Kumar Jha <bishalkmjha@gmail.com>
2025-09-19 17:04:12 +0530

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,7 +3,7 @@ import { type Database } from '@/database.types';
 
 export function createClient() {
   return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL = 'https://nzdpfgtafmmeyxuhbrhw.supabase.co'!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56ZHBmZ3RhZm1tZXl4dWhicmh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyNjAxMDksImV4cCI6MjA3MzgzNjEwOX0.lkk1VtVqaWdJArOyEmgKbpa8CJq9rN5cvVZVawC9pfU!
   )
 }
