d423cfd0d1f049e5d9e476db035034358b407d6e
Bishal Kumar Jha <bishalkmjha@gmail.com>
2025-09-19 17:33:45 +0530

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,9 +1,9 @@
-import { createBrowserClient } from '@supabase/ssr'
-import { type Database } from '@/database.types';
-
-export function createClient() {
-  return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL = 'https://nzdpfgtafmmeyxuhbrhw.supabase.co'!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56ZHBmZ3RhZm1tZXl4dWhicmh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyNjAxMDksImV4cCI6MjA3MzgzNjEwOX0.lkk1VtVqaWdJArOyEmgKbpa8CJq9rN5cvVZVawC9pfU!
-  )
+// GOOD - create client lazily inside function
+import { createClient } from '@supabase/supabase-js'
+export function getClient() {
+  return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+}
+export async function fetchStuff() {
+  const supabase = getClient()
+  return await supabase.from('x').select('*')
 }
